8e368e254bac891f7f2499b3dda11ed08ccb4aaf
mouhcine <mouhcine15.00@gmail.com>
2022-07-07 16:05:17 +0200

    costruttore

--- Movie.php
+++ Movie.php
@@ -3,5 +3,15 @@
 class Movie {
     public $titolo;
     public $regista;
+
+
+    public function __construct($_titolo, $_regista)
+    {
+        $this->titolo = $_titolo;
+        $this->regista = $_regista;
+    }
 }
+
+
+
 
--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 include __DIR__ . '/Movie.php';
 
-$matrix = new Movie();
-$matrix-> titolo = "Matrix";
-$matrix-> regista = "Andy Wachowski";
+$matrix = new Movie('Matrix', 'Andy Wachowski');
 
 var_dump($matrix);
+
+$titanic = new Movie('Titanic', 'James Cameron');
+
+var_dump($titanic);
+
+
+
+
+
+
+// $matrix-> titolo = "Matrix";
+// $matrix-> regista = "Andy Wachowski";

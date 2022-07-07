e7260d43500ef898656620e1c64d8adddaa6d9c8
mouhcine <mouhcine15.00@gmail.com>
2022-07-07 17:33:12 +0200

    metodo - esercizio completo

--- Movie.php
+++ Movie.php
@@ -3,12 +3,33 @@
 class Movie {
     public $titolo;
     public $regista;
+    public $anno;
+    public $rating;
+    public $best;
 
 
-    public function __construct($_titolo, $_regista)
+    public function __construct($_titolo, $_regista, $_anno, $_rating)
     {
         $this->titolo = $_titolo;
         $this->regista = $_regista;
+        $this->anno = $_anno;
+        $this->rating = $_rating;
+        $this->fanVote();
+    }
+
+    public function fanVote() 
+    {
+        if ($this->rating > 8.4) {
+            $this->best = 'TOP MOVIE';
+        } else if ($this->rating < 8.5 and $this->rating > 7.4) {
+            $this->best = 'VERY GOOD MOVIE';
+        }
+         else if ($this->rating < 7.5 || $this->rating > 5.9) {
+            $this->best = 'GOOD MOVIE';
+        }
+        else  {
+            $this->best = 'FLOP MOVIE';
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -1,18 +1,104 @@
 <?php
 include __DIR__ . '/Movie.php';
 
-$matrix = new Movie('Matrix', 'Andy Wachowski');
+$matrix = new Movie('Matrix', 'Andy Wachowski', '1999', 8.7);
 
-var_dump($matrix);
+$titanic = new Movie('Titanic', 'James Cameron', '1997', 7.9);
 
-$titanic = new Movie('Titanic', 'James Cameron');
+$sogni = new Movie('I sogni segreti di Walter Mitty', 'Ben Stiller', '2013', 7.3);
 
-var_dump($titanic);
-
-
-
+$vicini = new Movie('Cattivi vicini 2', 'Nicholas Stoller', '2016', 5.7);
+?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-// $matrix-> titolo = "Matrix";
-// $matrix-> regista = "Andy Wachowski";
+    <div>
+        <h3>
+            Titolo: <?= $matrix->titolo ?>
+        </h3>
+        <div>
+            Regista: <?= $matrix->regista ?>
+        </div>
+        <div>
+            Anno: <?= $matrix->anno ?>
+        </div>
+        <div>
+            IMDb rating: <?= $matrix->rating?>/10
+        </div>
+        <div>
+            Our site Rating: <?= $matrix->best?>
+        </div>
+
+    </div>
+
+    <div>
+        <h3>
+            Titolo: <?= $titanic->titolo ?>
+        </h3>
+        <div>
+            Regista: <?= $titanic->regista ?>
+        </div>
+        <div>
+            Anno: <?= $titanic->anno ?>
+        </div>
+        <div>
+            IMDb rating: <?= $titanic->rating?>/10
+        </div>
+        <div>
+            Our site Rating: <?= $titanic->best?>
+        </div>
+
+    </div>
+    
+    <div>
+        <h3>
+            Titolo: <?= $sogni->titolo ?>
+        </h3>
+        <div>
+            Regista: <?= $sogni->regista ?>
+        </div>
+        <div>
+            Anno: <?= $sogni->anno ?>
+        </div>
+        <div>
+            IMDb rating: <?= $sogni->rating?>/10
+        </div>
+        <div>
+            Our site Rating: <?= $sogni->best?>
+        </div>
+
+    </div>
+
+    <div>
+        <h3>
+            Titolo: <?= $vicini->titolo ?>
+        </h3>
+        <div>
+            Regista: <?= $vicini->regista ?>
+        </div>
+        <div>
+            Anno: <?= $vicini->anno ?>
+        </div>
+        <div>
+            IMDb rating: <?= $vicini->rating?>/10
+        </div>
+        <div>
+            Our site Rating: <?= $vicini->best?>
+        </div>
+
+    </div>
+    
+
+    
+</body>
+</html>
+
